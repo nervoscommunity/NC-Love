ed3b05746f08f4edd4af0481d0864d51ca22a75f
Orange <i@orangem.io>
2019-08-01 23:13:10 +0800

    fix

--- src/Top.vue
+++ src/Top.vue
@@ -5,14 +5,14 @@
             <div className="grid-content bg-purple-dark">
                 <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
                     <el-menu-item index="1">活动</el-menu-item>
-                    <el-menu-item index="2">新闻</el-menu-item>
+                    <el-menu-item disabled index="2">新闻</el-menu-item>
                 <!--<el-submenu index="2">
                         <template slot="title">新闻</template>
                         <el-menu-item index="2-1">NC News</el-menu-item>
                         <el-menu-item index="2-2">NC Article</el-menu-item>
                     </el-submenu>-->
-                    <el-menu-item index="3">快讯</el-menu-item>
-                    <el-menu-item index="4">投票</el-menu-item>
+                    <el-menu-item disabled index="3">快讯</el-menu-item>
+                    <el-menu-item disabled index="4">投票</el-menu-item>
                     <el-menu-item index="5">关于</el-menu-item>
                 </el-menu>
               </div>
